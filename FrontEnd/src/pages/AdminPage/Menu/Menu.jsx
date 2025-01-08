0386255e633393cb0b0267005008496a1e64c179
Lvyshnevska <lilia.vyshnevska@gmail.com>
2025-01-08 12:13:55 +0100

    replaced key in menu items list

--- FrontEnd/src/pages/AdminPage/Menu/Menu.jsx
+++ FrontEnd/src/pages/AdminPage/Menu/Menu.jsx
@@ -4,27 +4,22 @@ import css from './Menu.module.css';
 
 const MENU = [
     {
-        id: 'am1',
         title: 'Керування користувачами',
         link: '/customadmin/users/'
     },
     {
-        id: 'am2',
         title: 'Керування компаніями',
         link: '/customadmin/profiles/'
     },
     {
-        id: 'am3',
         title: 'Керування контактами',
         link: '/customadmin/contacts/'
     },
     {
-        id: 'am4',
         title: 'Налаштування часу автоапруву',
         link: '/customadmin/automoderation/'
     },
     {
-        id: 'am5',
         title: 'Статистика компаній',
         link: '/customadmin/statistics/'
     },
@@ -33,12 +28,10 @@ const MENU = [
 
 const SUPERUSER_MENU = [
     {
-        id: 'am5',
         title: 'Пошта адміністратора',
         link: '/customadmin/email/'
     },
     {
-        id: 'am6',
         title: 'Реєстрація адміністратора',
         link: '/customadmin/admin-create/'
     }
@@ -58,7 +51,7 @@ function Menu() {
                 {menuItems.map((element) => (
                     <NavLink
                         className={({ isActive }) => (`${css['menu-section-element']} ${isActive && css['menu-section-element__active']}`)}
-                        key={element.id} to={element.link}>{element.title}
+                        key={element.title} to={element.link}>{element.title}
                     </NavLink>
                 ))}
             </div>
