cca54f2ea491389f9617aadc274ed2f1e367e1f0
Leonid Chelakhov <leonidchelahov@gmail.com>
2025-01-19 15:43:06 +0300

    Refactored some code

--- FrontEnd/src/pages/ProfilePage/Mobile/EditProfileMobile.jsx
+++ FrontEnd/src/pages/ProfilePage/Mobile/EditProfileMobile.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useAuth, useProfile } from '../../../hooks';
+import PropTypes from 'prop-types';
 import { validateRequiredFields, REQUIRED_FIELDS_GENERAL_INFO } from '../../../utils/validateRequiredFields';
 import NotificationBanner from '../FormComponents/NotificationBanner';
 import Accordion from './Accordion';
@@ -14,59 +15,59 @@ import ChangePassword from '../FormComponents/ChangePassword';
 import Loader from '../../../components/Loader/Loader';
 import css from './EditProfileMobile.module.css';
 
-const EditProfileMobile = () => {
+const EditProfileMobile = ({ openSection, setOpenSection }) => {
   const { user } = useAuth();
   const { profile } = useProfile();
   const [missingFields, setMissingFields] = useState([]);
-  const [openSection, setOpenSection] = useState(null);
 
   useEffect(() => {
     if (profile && user) {
       const generalInfoMissing = validateRequiredFields(profile, user)
         .filter(field => REQUIRED_FIELDS_GENERAL_INFO.includes(field));
 
-      setMissingFields([...generalInfoMissing ]);
+      setMissingFields([...generalInfoMissing]);
     }
   }, [profile, user]);
+
   if (user && profile) {
     const sections = [
       {
         title: 'Інформація про користувача',
-        content: <UserInfo user={user}
-          profile={profile} />
+        content: <UserInfo user={user} profile={profile} />,
       },
       {
         title: 'Загальна інформація',
-        content: <GeneralInfo profile={profile} />
+        content: <GeneralInfo profile={profile} />,
       },
       {
         title: 'Контакти',
-        content: <ContactsInfo profile={profile} />
+        content: <ContactsInfo profile={profile} />,
       },
       {
         title: 'Інформація про товари/послуги',
         content: <ProductServiceInfo profile={profile} />,
-        disabled: !profile.is_registered
+        disabled: !profile.is_registered,
       },
       {
         title: 'Додаткова інформація',
         content: <AdditionalInfo profile={profile} />,
-        disabled: !profile.is_registered
+        disabled: !profile.is_registered,
       },
       {
         title: 'Стартап',
         content: <StartupInfo profile={profile} />,
-        disabled: !profile.is_startup
+        disabled: !profile.is_startup,
       },
       {
         title: 'Змінити пароль',
-        content: <ChangePassword user={user} />
+        content: <ChangePassword user={user} />,
       },
       {
         title: 'Видалити профіль',
-        content: <DeleteProfilePage />
+        content: <DeleteProfilePage />,
       },
     ];
+
     return (
       <div className={css['container']}>
         {missingFields.length > 0 && (
@@ -79,6 +80,11 @@ const EditProfileMobile = () => {
   } else {
     return <Loader />;
   }
+};
+
+EditProfileMobile.propTypes = {
+  openSection: PropTypes.any,
+  setOpenSection: PropTypes.func.isRequired,
 };
 
 export default EditProfileMobile;
