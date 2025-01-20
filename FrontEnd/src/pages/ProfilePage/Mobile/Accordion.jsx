0ddbfdb39e8e8a50adc36739045d0e1901574435
Lvyshnevska <lilia.vyshnevska@gmail.com>
2025-01-20 15:05:35 +0100

    fixed bug with warning - reset inputs, open new section, collapse previous

--- FrontEnd/src/pages/ProfilePage/Mobile/Accordion.jsx
+++ FrontEnd/src/pages/ProfilePage/Mobile/Accordion.jsx
@@ -5,12 +5,30 @@ import WarnUnsavedDataModal from '../FormComponents/WarnUnsavedDataModal';
 import { DirtyFormContext } from '../../../context/DirtyFormContext';
 
 const Accordion = ({ sections }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const [previousIndex, setPreviousIndex] = useState(null);
+  const [targetIndex, setTargetIndex] = useState(null);
+  const [triggerKey, setTriggerKey] = useState(0);
   const [showWarningModal, setShowWarningModal] = useState(false);
-  const {  setFormIsDirty } = useContext(DirtyFormContext);
+  const {  formIsDirty, setFormIsDirty } = useContext(DirtyFormContext);
+
+  const handleItemClick = (index, disabled) => {
+    if (disabled) return;
+
+    if (!formIsDirty) {
+        setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
+        } else {
+          setShowWarningModal(true);
+          setTargetIndex(index);
+        }
+    };
 
   const onConfirmModal = () => {
     setShowWarningModal(false);
     setFormIsDirty(false);
+    setPreviousIndex(activeIndex);
+    setActiveIndex(targetIndex);
+    setTriggerKey((prev) => prev + 1);
   };
 
   const onCancelModal = () => {
@@ -20,13 +38,16 @@ const Accordion = ({ sections }) => {
   return (
     <>
       <div className="accordion">
-        {sections.map((section) => (
+        {sections.map((section, index) => (
           <AccordionItem
-            key={section.title}
+            key={index}
             title={section.title}
             content={section.content}
             disabled={section.disabled}
-            warningHandler={() => setShowWarningModal(true)} />
+            isOpen={activeIndex === index}
+            triggerKey={previousIndex === index ? triggerKey : null}
+            onClick={() => handleItemClick(index, section.disabled)}
+          />
         ))}
       </div>
       <MyModal visible={showWarningModal}>
--- FrontEnd/src/pages/ProfilePage/Mobile/AccordionItem.jsx
+++ FrontEnd/src/pages/ProfilePage/Mobile/AccordionItem.jsx
@@ -1,28 +1,18 @@
-import { useState, useContext } from 'react';
 import css from './AccordionItem.module.css';
-import { DirtyFormContext } from '../../../context/DirtyFormContext';
 
 const AccordionItem = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const { formIsDirty } = useContext(DirtyFormContext);
-
-    const toggle = () => {
-        if (!props.disabled) {
-            formIsDirty ? props.warningHandler() : setIsOpen(!isOpen);
-        }
-    };
-
     return (
         <div className={css['accordion-item']}>
             <button className={`${css['accordion-button']} ${props.disabled ? css['disabled'] : ''}`}
-                    onClick={toggle}>
+                    onClick={props.onClick}
+                    >
                 <p className={props.title === 'Видалити профіль' ? css['danger'] : ''}>
                     {props.title}
                 </p>
-               {!props.disabled && <img src={`${process.env.REACT_APP_PUBLIC_URL}/svg/arrow-${isOpen ? 'up' : 'down'}.svg`}></img>}
+               {!props.disabled && <img src={`${process.env.REACT_APP_PUBLIC_URL}/svg/arrow-${props.isOpen ? 'up' : 'down'}.svg`}></img>}
             </button>
             <div className={css['divider']}></div>
-            <div className={`${css['accordion-content']} ${isOpen ? '' : css['close']}`}>
+            <div key={props.triggerKey} className={`${css['accordion-content']} ${props.isOpen ? '' : css['close']}`}>
                 {props.content}
             </div>
         </div>
