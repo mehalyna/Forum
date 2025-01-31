e4f45919f557967f7d889f31c83d70e606e7c14c
Andrewakiv <danchyshy.andriy@gmail.com>
2025-01-31 03:29:25 +0200

    deleted breadcrumbs and fixed admin page title

--- FrontEnd/src/pages/AdminPage/AdminLayout.jsx
+++ FrontEnd/src/pages/AdminPage/AdminLayout.jsx
@@ -6,13 +6,13 @@ const AdminLayout = ({ children }) => {
     return (
       <div className={css['admin-layout__wrapper']}>
         <div className={css['admin-layout']}>
-            <NavLink className={css['admin-layout__title']} to={'/customadmin/'}>
-                <h2 className={css['admin-layout__title--text']}>Панель адміністратора</h2>
-            </NavLink>
-              <div className={css['admin-layout__content']}>
-                <Menu />
-                  <div className={css['admin-layout__children-section']}>{children}</div>
-            </div>
+          <NavLink className={css['admin-layout__title']} to={'/customadmin/'}>
+            <h2 className={css['admin-layout__title--text']}>Панель адміністратора</h2>
+          </NavLink>
+          <div className={css['admin-layout__content']}>
+            <Menu />
+            <div className={css['admin-layout__children-section']}>{children}</div>
+          </div>
         </div>
       </div>
     );
