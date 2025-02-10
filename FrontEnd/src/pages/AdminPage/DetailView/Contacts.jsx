0e1a60043faa30be2aabaa2152359e6db7a64e37
KyzukY <kyzuk.yr@gmail.com>
2025-02-10 10:37:42 +0200

    add mask and errors message

--- FrontEnd/src/pages/AdminPage/DetailView/Contacts.jsx
+++ FrontEnd/src/pages/AdminPage/DetailView/Contacts.jsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import css from'./Contacts.module.css';
+import css from './Contacts.module.css';
+import { useMask } from '@react-input/mask';
 
-const LoadingMessage = () => <div className="loading">Завантаження...</div>;
+const errorTranslation = {
+    'Enter a valid email address.': 'Введіть коректний email.',
+    'Phone number must be exactly 12 characters long.': 'Номер телефону має містити рівно 12 цифр.',
+    'Phone number must contain only numbers.': 'Номер телефону має містити лише цифри.',
+    'Address must contain only alphanumeric characters and valid symbols (e.g., commas, periods).':
+        'Адреса має містити тільки букви, цифри та допустимі символи (, .).',
+    'Company name contains invalid characters. Only Ukrainian and Latin letters, numbers, and specific symbols are allowed.':
+        'Назва компанії містить недопустимі символи. Дозволені лише українські та латинські літери, цифри та деякі символи.'
+};
 
 const Contacts = () => {
     const [contacts, setContacts] = useState({});
     const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [error, setError] = useState({});
     const [success, setSuccess] = useState(false);
 
     useEffect(() => {
@@ -17,7 +26,7 @@ const Contacts = () => {
                 setContacts(response.data);
             } catch (error) {
                 console.error('Error fetching contacts:', error);
-                setError('Не вдалося завантажити контактну інформацію.');
+                setError({ general: 'Не вдалося завантажити контактну інформацію.' });
             } finally {
                 setLoading(false);
             }
@@ -25,6 +34,8 @@ const Contacts = () => {
 
         fetchContacts();
     }, []);
+
+    const phoneMaskRef = useMask({ mask: '380XXXXXXXXX', replacement: { X: /\d/ } });
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -34,23 +45,30 @@ const Contacts = () => {
         }));
     };
 
-
     const handleSubmit = async (e) => {
         e.preventDefault();
         setSuccess(false);
-        setError(null);
+        setError({});
 
         try {
             await axios.put(`${process.env.REACT_APP_BASE_API_URL}/api/admin/contacts/`, contacts);
             setSuccess(true);
         } catch (error) {
             console.error('Error updating contacts:', error);
-            setError('Не вдалося зберегти зміни.');
+            if (error.response && error.response.data) {
+                const translatedErrors = Object.keys(error.response.data).reduce((acc, key) => {
+                    acc[key] = error.response.data[key].map(msg => errorTranslation[msg] || msg);
+                    return acc;
+                }, {});
+                setError(translatedErrors);
+            } else {
+                setError({ general: 'Не вдалося зберегти зміни.' });
+            }
         }
     };
 
-    if (loading) return <LoadingMessage />;
-    if (error && !contacts) return <div className={css['error']}>{error}</div>;
+    if (loading) return <div className={css['loading']}>Завантаження...</div>;
+    if (error.general) return <div className={css['error']}>{error.general}</div>;
 
     return (
         <div className={css['contacts-container']}>
@@ -67,6 +85,7 @@ const Contacts = () => {
                             onChange={handleInputChange}
                             required
                         />
+                        {error.company_name && <p className={css['error-message']}>{error.company_name}</p>}
                     </label>
                     <label className={css['form-label']}>
                         Адреса:
@@ -78,6 +97,7 @@ const Contacts = () => {
                             onChange={handleInputChange}
                             required
                         />
+                        {error.address && <p className={css['error-message']}>{error.address}</p>}
                     </label>
                 </div>
                 <div className={css['form-row']}>
@@ -91,6 +111,7 @@ const Contacts = () => {
                             onChange={handleInputChange}
                             required
                         />
+                        {error.email && <p className={css['error-message']}>{error.email}</p>}
                     </label>
                     <label className={css['form-label']}>
                         Телефон:
@@ -98,10 +119,13 @@ const Contacts = () => {
                             className={css['form-input']}
                             type="text"
                             name="phone"
+                            ref={phoneMaskRef}
+                            placeholder="380XXXXXXXXX"
                             value={contacts?.phone || ''}
                             onChange={handleInputChange}
                             required
                         />
+                        {error.phone && <p className={css['error-message']}>{error.phone}</p>}
                     </label>
                 </div>
                 <div className={css['form-buttons']}>
@@ -109,7 +133,6 @@ const Contacts = () => {
                 </div>
             </form>
             {success && <p className={css['success-message']}>Зміни успішно збережені!</p>}
-            {error && <p className={css['error-message']}>{error}</p>}
         </div>
     );
 };
