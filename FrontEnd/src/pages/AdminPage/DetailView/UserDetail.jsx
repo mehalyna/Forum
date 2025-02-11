909c58c19af05af9b58084d3a9054912a2daf401
Alex <alexandersychev2005@gmail.com>
2025-02-11 12:03:31 +0200

    Adjusted detailed description for users in admin panel

--- FrontEnd/src/pages/AdminPage/DetailView/UserDetail.jsx
+++ FrontEnd/src/pages/AdminPage/DetailView/UserDetail.jsx
@@ -1,113 +1,81 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router';
 import css from './UserDetail.module.css';
-import DeleteModal from './DeleteModal';
 import axios from 'axios';
 import useSWR from 'swr';
+import {Badge, Descriptions} from 'antd';
+import Loader from '../../../components/Loader/Loader';
 
 function UserDetail() {
-    const [deleteModalActive, setDeleteModalActive] = useState(false);
     const [user, setUser] = useState({});
     const userId = usePathUserId();
-    const [updateSuccess, setUpdateSuccess] = useState(false);
     const url = `${process.env.REACT_APP_BASE_API_URL}/api/admin/users/${userId}/`;
-    const navigateToUsers = useNavigate();
-
     const fetcher = url => axios.get(url).then(res => res.data);
-    const { data, error, isValidating: loading } = useSWR(url, fetcher);
+    const {data, error, isLoading} = useSWR(url, fetcher);
     if (data && !Object.keys(user).length) {
         setUser(data);
     }
-
-    const handleSaveChanges = async () => {
-        const response = await axios.put(
-            url,
-            {
-                name: user.name,
-                surname: user.surname,
-                email: user.email,
-                is_active: user.is_active,
-                is_staff: user.is_staff,
-                is_superuser: user.is_superuser
-            },
-        );
-        if (response.status !== 200) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        setUpdateSuccess(true);
-        setTimeout(() => setUpdateSuccess(false), 3000);
-    };
-
-    const handleDeleteUser = async () => {
-        const response = await axios.delete(url);
-        if (response.status !== 204) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        setUser({});
-        navigateToUsers('/customadmin/users');
-    };
-
+    const items = user ? [
+        {
+            key: '1',
+            label: 'Ім\'я',
+            children: user.name
+        },
+        {
+            key: '2',
+            label: 'Прізвище',
+            children: user.surname
+        },
+        {
+            key: '3',
+            label: 'Пошта',
+            children: user.email
+        },
+        {
+            key: '4',
+            label: 'Чи заблокований користувач?',
+            children: (
+                <Badge
+                    status={user.status === 'blocked' ? 'error' : 'success'}
+                    text={user.status === 'blocked' ? 'Заблокованний' : 'Активний'}
+                />
+            ),
+            span: 2
+        },
+        {
+            key: '5',
+            label: 'Чи користувач робітник?',
+            children: (
+                <Badge
+                    status={user.is_staff === 'True' ? 'error' : 'success'}
+                    text={user.is_staff === 'True' ? 'Ні' : 'Так'}
+                />
+            ),
+            span: 2
+        },
+        {
+            key: '5',
+            label: 'Чи користувач суперюзер?',
+            children: (
+                <Badge
+                    status={user.is_superuser === 'True' ? 'error' : 'success'}
+                    text={user.is_superuser === 'True' ? 'Ні' : 'Так'}
+                />
+            ),
+            span: 2
+        },
+    ] : [];
     return (
-        <div className={css['user-detail-page']}>
-            <DeleteModal
-                active={deleteModalActive}
-                setActive={setDeleteModalActive}
-                onDelete={handleDeleteUser}
-            />
-            <div className={css['user-details-section']}>
-                <ul className={css['log-section']}>
-                    {loading && <li className={css['log']} >Завантаження ...</li>}
-                    {error && <li className={css['log']}>Виникла помилка: {error}</li>}
-                    {updateSuccess && <li className={css['log']}>Користувач успішно оновлений!</li>}
-                </ul>
-                <ul className={css['form-info__user_text']}>
-                    <li>Ім&apos;я: {user.name || ''}</li>
-                    <li>Прізвище: {user.surname || ''}</li>
-                    <li>Email: {user.email || ''}</li>
-                    <li>Активний користувач: {user.is_active ? 'Так' : 'Ні'}</li>
-                    <li>Персонал: {user.is_staff ? 'Так' : 'Ні'}</li>
-                    <li>Компанія: {user.company_name ? 'Так' : 'Ні'}</li>
-                </ul>
-                <div className={css['form-section']}>
-                    <label className={css['form-info__text']}>Прізвище</label>
-                    <input
-                        value={user.surname || ''}
-                        onChange={(e) => setUser({ ...user, surname: e.target.value })}
-                        type="text"
-                        className={css['form-input']}
-                    />
-                    <label className={css['form-info__text']}>Ім&apos;я</label>
-                    <input
-                        value={user.name || ''}
-                        onChange={(e) => setUser({ ...user, name: e.target.value })}
-                        type="text"
-                        className={css['form-input']}
-                    />
-                    <label className={css['form-info__text']}>Активний користувач</label>
-                    <input
-                        type="checkbox"
-                        checked={user.is_active || false}
-                        onChange={(e) => setUser({ ...user, is_active: e.target.checked })}
-                        className={css['form-input_checkbox']}
-                    />
+        (error && error.status !== 401
+        ) ? (
+            <div className={css['log']}>Виникла помилка: {error.message}</div>
+        ) : (
+            isLoading ? <Loader /> :
+            <div className={css['profile-detail-page']}>
+                <div className={css['profile-details-section']}>
+                    <Descriptions title="Детальна інформація користувача" bordered items={items} column={2}/>
                 </div>
-                <button className={css['save-button']} onClick={handleSaveChanges}>Зберегти зміни</button>
             </div>
-            <div className={css['delete-section']}>
-                <div className={css['form-info__text']}>
-                    {user.company_name ? 'Заборона на видалення, користувач має профіль' : 'Видалити акаунт'}
-                </div>
-                {!user.company_name && (
-                    <button
-                        className={css['button__delete']}
-                        onClick={() => setDeleteModalActive(true)}
-                        disabled={user.company_name}
-                    >
-                        Видалити
-                    </button>
-                )}
-            </div>
-        </div>
+        )
     );
 }
 function usePathUserId() {
