5ae92c9a3d7019fd9625e78a35f1134388494d14
Leonid Chelakhov <leonidchelahov@gmail.com>
2025-02-13 17:48:39 +0300

    Added a function in pagewrapper

--- FrontEnd/src/components/PageWrapper/PageWrapper.jsx
+++ FrontEnd/src/components/PageWrapper/PageWrapper.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useBurgerMenu } from '../../context/BurgerMenuContext';
 import { useCookieContext } from '../../context/CookieContext';
@@ -10,6 +11,26 @@ const PageWrapper = ({ children }) => {
   const { isCookieModalActive } = useCookieContext();
 
   useScrollToHash();
+
+  useEffect(() => {
+    const updatePageContentHeight = () => {
+      const header = document.querySelector('header');
+      const footer = document.querySelector('footer');
+
+      const headerHeight = header ? header.offsetHeight : 0;
+      const footerHeight = footer ? footer.offsetHeight : 0;
+
+      document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
+      document.documentElement.style.setProperty('--footer-height', `${footerHeight}px`);
+    };
+
+    updatePageContentHeight();
+    window.addEventListener('resize', updatePageContentHeight);
+
+    return () => {
+      window.removeEventListener('resize', updatePageContentHeight);
+    };
+  }, []);
 
   return (
     <div className={`${css.pageContent} ${isOpen ? css.menuOpen : ''}`}>
