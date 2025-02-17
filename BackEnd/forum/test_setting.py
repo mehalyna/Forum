cf298f22a39a34c0589ab1897ceb23e3a2f71807
Lvyshnevska <lilia.vyshnevska@gmail.com>
2025-02-17 11:53:44 +0100

    reduced delay period for login in tests

--- BackEnd/forum/test_setting.py
+++ BackEnd/forum/test_setting.py
@@ -1,7 +1,7 @@
 from .settings import *
 
 PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
-DELAY_FOR_LOGIN = 5
+DELAY_FOR_LOGIN = 3
 ATTEMPTS_FOR_LOGIN = 2
 DELAY_FOR_UPLOADS = 600
 MAX_UPLOADS = 1
